5e033031c5a93157d4530c4d9c60e3ad438b3efa
ivo <ivostoianov94@gmail.com>
2015-03-30 23:47:48 +0300

    Pretify and minor bug fix

--- HW6/TODO-List-improved/scripts/app.js
+++ HW6/TODO-List-improved/scripts/app.js
@@ -10,31 +10,28 @@ var todoList = todoList || {};
     }
 
     function run() {
-        var fragment = new DocumentFragment();
-        var container = document.createElement("div");
+        var fragment = new DocumentFragment(),
+            container = document.createElement("div"),
+            body = document.getElementsByTagName('body')[0],
+            buttonWrapper = document.createElement('div'),
+            inputText = document.createElement('input'),
+            inputButton = document.createElement('input');
+
         container.setAttribute('id', 'container');
-
-        var buttonWrapper = document.createElement('div');
         buttonWrapper.setAttribute('id', 'buttonWrapper');
-
-        var inputText = document.createElement('input');
         inputText.setAttribute('type', 'text');
         inputText.setAttribute('id', 'createCont');
         inputText.setAttribute('placeholder', 'Create New Container');
-
-        var inputButton = document.createElement('input');
         inputButton.setAttribute('type', 'button');
         inputButton.setAttribute('value', 'New Container');
-        inputButton.onclick = createContainer;
 
+        inputButton.onclick = createContainer;
 
         buttonWrapper.appendChild(inputText);
         buttonWrapper.appendChild(inputButton);
         buttonWrapper.appendChild(document.createElement('br'));
         container.appendChild(buttonWrapper);
         fragment.appendChild(container);
-        var body = document.getElementsByTagName('body')[0];
-        console.log(body);
         body.appendChild(fragment);
     }
 
